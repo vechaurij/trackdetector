d86cbb147c9caffbd54035fdf80081bbcc5d7b39
Virgilio Echauri Jiménez <vechauridev@gmail.com>
2020-07-12 12:34:39 +0200

    Corrected bug, code refactor, clieaning and documentation,

--- contourUtil.py
+++ contourUtil.py
@@ -1,44 +1,74 @@
 import numpy as np
 import cv2
 import math
+from enum import Enum
 
-def getContoursIndexesAndAreaOrderedByArea(contours):
+class ContourOrderFeature(Enum):
+    AREA = 1
+    SIZE = 2
 
+def getContoursIndexesOrderedByFeature(contours, contourFeature = ContourOrderFeature.AREA):
+    """Given a set of contours found by cv2.findContours function, 
+        returns a ndarray containing the contours indexes ordered by a contour feature 
+        (Contour Area or Contour Size) selected by the parameter contourFeature.
+        The returned array will be of shape nx3 of integers, given that there are n contours.
+        
+        For each tuple on the returned array, each column represent the following
+        0: Tuple number of the returned array, from 0 to n, where n is the number of contours.
+        1: Contour area or contour size of one of the contours
+        2: Index of the contours with area or size indicated on column 1 on the input contours array
+
+        Parameters
+        ----------
+        contours : ndarray
+            Array returned by the function cv2.findContours containing all contours found on an image
+        contourFeature: Enum
+            Indicates the contour feature to be used to order the returned array, two possibilities
+            ContourOrderFeature.AREA (Default if no parameter set): The area of the contour, given by cv2.contourArea
+            ContourOrderFeature.SIZE: The size of the contour measured in pixels. The "method" parameter of the 
+                cv2.findContours function must be set to cv2.CHAIN_APPROX_NONE in order to get all the points of the contour
+
+        Raises
+        ------
+        Nothing.
+    """
+    
     contoursNum = np.size(contours)
-
-    contoursIndexesWithArea = np.empty(contoursNum*3,dtype='i').reshape(contoursNum,3)
+    contoursIndexesWithFeature = np.empty(contoursNum*3,dtype='i').reshape(contoursNum,3)
  
     for i in range(contoursNum):
-        contoursIndexesWithArea[i,1] = i
-        contoursIndexesWithArea[i,2] = cv2.contourArea(contours[i])
+        if contourFeature is ContourOrderFeature.AREA:
+            contoursIndexesWithFeature[i,1] = cv2.contourArea(contours[i])
+        else:
+            contoursIndexesWithFeature[i,1] = np.size(contours[i])    
+        contoursIndexesWithFeature[i,2] = i
 
-    # Order the contoursIndexesWithArea array by it's second column
-    contoursIndexesWithAreaOrder = contoursIndexesWithArea[contoursIndexesWithArea[:,2].argsort()]
+    # Order the contoursIndexesWithFeature array by it's second column where the ordering 
+    # feature of the contours is (AREA or SIZE)
+    contoursIndexesWithFeatureOrder = contoursIndexesWithFeature[contoursIndexesWithFeature[:,1].argsort()]
 
-    contoursIndexesWithAreaOrder[:,0] = range(contoursNum)
+    # Fill the first column with numbers from 0 to n (number of contours)
+    contoursIndexesWithFeatureOrder[:,0] = range(contoursNum)
 
-    return contoursIndexesWithAreaOrder
+    return contoursIndexesWithFeatureOrder
 
-
-def getContoursIndexesAndSizeOrderedBySize(contours):
-
-    contoursNum = np.size(contours)
-
-    contoursIndexesWithSize = np.empty(contoursNum*3,dtype='i').reshape(contoursNum,3)
- 
-    for i in range(contoursNum):
-        contoursIndexesWithSize[i,1] = i
-        contoursIndexesWithSize[i,2] = np.size(contours[i])
-
-    # Order the contoursIndexesWithArea array by it's second column
-    contoursIndexesWithSizeOrder = contoursIndexesWithSize[contoursIndexesWithSize[:,2].argsort()]
-
-    contoursIndexesWithSizeOrder[:,0] = range(contoursNum)
-
-    return contoursIndexesWithSizeOrder
-
-
-def removeImagePerimeterContour(img, contours):
+def removeImagePerimeterContour(img, contours, percentArea = 0.8):
+    """Given an image, its contours found by the cv2.findContours function, removes
+        the first contour found whose area is larger than the percentage set of the image
+        area set by the percentArea parameter
+        Parameters
+        ----------
+        imb : ndarray
+            Array containing the original image
+        contours : ndarray
+            Array returned by the function cv2.findContours containing all contours found on an image
+        percentArea : float
+            Percentage of area of the source img to determine the contour to remove if it's larger. By default 80% (Or 0.8)
+            
+        Raises
+        ------
+        Nothing.
+    """
 
     height,width = img.shape
 
@@ -47,7 +77,7 @@ def removeImagePerimeterContour(img, contours):
     imageArea = height * width
 
     for i in range(contourNum):
-        if ((cv2.contourArea(contours[i]) / imageArea) > 0.80):
+        if ((cv2.contourArea(contours[i]) / imageArea) > percentArea):
             contourFound = True
             break
 
--- trackDetector.py
+++ trackDetector.py
@@ -9,81 +9,87 @@ from contourUtil import *
 
 def findImageContours(imageFile):
 
-    # Set image size
+    # Read the image from filesystem given by imageFile parameter
+    img = cv2.imread(imageFile, cv2.IMREAD_UNCHANGED)
+
+    # Set image size to work with
     imageSize = (800, 600)
+
+    # Resize the read image to the working size defined
+    imgResized = cv2.resize(img, imageSize)
+
+    # Convert img to grey scale
+    imgGrey = cv2.cvtColor(imgResized,cv2.COLOR_BGR2GRAY)
 
     # Set the kernel size for image blur
     ksize = (5, 5) 
 
-    img = cv2.imread(imageFile, cv2.IMREAD_UNCHANGED)
-
-    # resize image
-    imgResized = cv2.resize(img, imageSize)
-
-    #convert img to grey
-    imgGrey = cv2.cvtColor(imgResized,cv2.COLOR_BGR2GRAY)
-
+    # Blur the image with the definez kernel size
     imgBlur = cv2.blur(imgGrey,ksize)
 
-    #set a thresh
+    # Set a threshold limit to transform the input image
     thresh = 80
 
-    #get threshold image
+    # Get threshold input image
     ret,imgThresh = cv2.threshold(imgBlur, thresh, 255, cv2.THRESH_BINARY)
 
     cv2.imshow("Threshold",imgThresh)
     cv2.waitKey()
 
-    #find contours
+    # Find contours on the thresholded image
     contours, hierarchy = cv2.findContours(imgThresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    contourNum = np.size(contours)
-
-    print("Contours found: " + str(contourNum))
-
+    # Remove the largest contour found if it could be the perimeter of the source image
     contours = removeImagePerimeterContour(imgThresh,contours)
 
     contourNum = np.size(contours)
-
     print("Contours found: " + str(contourNum))
 
-    contoursIndexesAndAreasOrderedByArea = getContoursIndexesAndAreaOrderedByArea(contours)
-    contoursIndexesAndSizesOrderedBySize = getContoursIndexesAndSizeOrderedBySize(contours)
+    # Get an array of shape nx3 containing: number, area of the contour represented by the index and the contour index 
+    contoursIndexesAndAreasOrderedByArea = getContoursIndexesOrderedByFeature(contours, ContourOrderFeature.AREA)
+
+    # Get an array of shape nx3 containing: number, size of the contour represented by the index and the contour index 
+    contoursIndexesAndSizesOrderedBySize = getContoursIndexesOrderedByFeature(contours, ContourOrderFeature.SIZE)
 
     print("Contours with indexes and areas: " + str(contoursIndexesAndAreasOrderedByArea))
     print("Contours with indexes and sizes: " + str(contoursIndexesAndSizesOrderedBySize))
+
+    # Initialize the kneebow library to get the point of the curves of contur sizes or areas where they start to grow exponentialy
     rotor = Rotor()
-    rotor.fit_rotate(contoursIndexesAndAreasOrderedByArea[:,1:3])
+
+    # Find the point of the curve containing all contour areas where it starts to grow exponentialy
+    rotor.fit_rotate(contoursIndexesAndAreasOrderedByArea[:,0:2])
     elbowIndexAreas = rotor.get_elbow_index()
 
-    rotor.fit_rotate(contoursIndexesAndSizesOrderedBySize[:,1:3])
+    # Find the point of the curve containing all contour sizes where it starts to grow exponentialy
+    rotor.fit_rotate(contoursIndexesAndSizesOrderedBySize[:,0:2])
     elbowIndexSizes = rotor.get_elbow_index()
 
     print("Elbow Index for areas: " + str(elbowIndexAreas))
     print("Elbow Index for sizes: " + str(elbowIndexSizes))
 
+    # The posssible contours candidates to be a track will start on the highest of the sizes or areas found
+    # in order to discard false contours not being tracks but with enough size.
     highestElbow = elbowIndexAreas
     if (elbowIndexSizes > elbowIndexAreas):
         highestElbow = elbowIndexSizes
 
-    #create an empty image for contours
+    # Create an empty image for drawing the contours
     imgContours = np.zeros(imgResized.shape)
 
-    # biggestGap = findBiggestGap(orderedContoursAreasByArea)
-
-    # print("Biggest Gap: " + str(biggestGap))
-
+    # Interate over all contours found but only draw those contours on the exponential grow part of the curves defined
+    # by sizes and areas of the found contours
     for x in range(contourNum):
         
-        print("Contour: " + str(contoursIndexesAndAreasOrderedByArea[x,1]) + " Area: " + str(contoursIndexesAndAreasOrderedByArea[x,2]) + "Size: " + str(contoursIndexesAndSizesOrderedBySize[x,2]))
+        print("Contour: " + str(contoursIndexesAndAreasOrderedByArea[x,2]) + " Area: " + str(contoursIndexesAndAreasOrderedByArea[x,1]) + "Size: " + str(contoursIndexesAndSizesOrderedBySize[x,1]))
 
+        # Take into account those contours present on the exponential part of the curve defined by sizes or areas of found contours
         if (x > highestElbow):
-            # draw the contours on the empty image
-            cv2.drawContours(imgContours, contours, contoursIndexesAndAreasOrderedByArea[x,1], (0,255,0), 3)
+            # Draw the contours on the empty image
+            cv2.drawContours(imgContours, contours, contoursIndexesAndAreasOrderedByArea[x,2], (0,255,0), 3)
 
-            #save image
+            # Display the contour over the empty image
             cv2.imshow("contoursFound",imgContours)
-
             cv2.waitKey()
 
 
